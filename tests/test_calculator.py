41b20eaf570f8192cc0166aba55dd70092fc6266
Dmitry Myasnikov <dmyasnikov@griddynamics.com>
2019-01-16 10:56:17 +0300

    Add unit tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,10 @@
+import unittest
+from calc.calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(Calculator.add(2,3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
+
