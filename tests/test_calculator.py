1e4ad614d2a19b249ac3290bd6dae5a4de323e8d
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-01-18 17:27:51 +0300

    I guess its ADSAFE-187, but its works so 188 (#4)
    * Test cases added for all arithmetic operations.
    
    ADSAFE-187
    
    * update test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,26 @@
 import unittest
 from calc.calculator import Calculator
 
-class TestCalculator(unittest.TestCase):
+
+class CalculatorTest(unittest.TestCase):
+    calculator = Calculator()
+
     def test_add(self):
-        self.assertEqual(Calculator.add(2,3), 5)
+        self.assertEqual(4, self.calculator.add(2, 2))
+        self.assertEqual(3.2, self.calculator.add(1, 2.2))
 
-if __name__ == '__main__':
+    def test_minus(self):
+        self.assertEqual(2, self.calculator.sub(3, 1))
+        self.assertEqual(-2, self.calculator.sub(1, 3))
+
+    def test_multiple(self):
+        self.assertEqual(12, self.calculator.mul(3, 4))
+        self.assertEqual(13.5, self.calculator.mul(3, 4.5))
+
+    def test_devide(self):
+        self.assertEqual(3, self.calculator.div(9, 3))
+        self.assertEqual(4, self.calculator.div(16, 4))
+
+
+if __name__ == "__main__":
     unittest.main()
-
