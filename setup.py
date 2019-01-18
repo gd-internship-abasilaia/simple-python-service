c00c55264ecf15d456b2b6fa22731293ca0cec62
Andrei Basilaia <idronandrew@gmail.com>
2019-01-18 11:58:57 +0300

    Test cases added for all arithmetic operations.
    ADSAFE-187

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name="clc",
+    version="0.1",
+    description='A useful module',
+    packages=find_packages(),
+    files=[
+        'waw.sh',
+        'README.md',]
+)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,20 @@
 import unittest
 from calc.calculator import Calculator
 
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(Calculator.add(2,3), 5)
+        self.assertEqual(Calculator.add(2, 3), 5)
+
+    def test_sub(self):
+        self.assertEqual(Calculator.sub(5, 2), 3)
+
+    def test_mul(self):
+        self.assertEqual(Calculator.mul(3, 2), 6)
+
+    def test_div(self):
+        self.assertEqual(Calculator.div(8, 2), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
-
