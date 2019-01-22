e9df65621dd31856bc1179b950adc7fea010525c
Andrei Basilaia <idronandrew@gmail.com>
2019-01-22 10:54:59 +0300

    Fixed test_calculator.py. Now it works without class instance
    ADSAFE-188

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,28 +3,26 @@ from calc.calculator import Calculator
 
 
 class TestCalculator(unittest.TestCase):
-    calculator = Calculator()
-
     def test_add(self):
-        self.assertEqual(4, self.calculator.add(2, 2))
-        self.assertNotEqual(-4, self.calculator.add(2, 2))
-        self.assertEqual(3.2, self.calculator.add(1, 2.2))
-        self.assertEqual(-6, self.calculator.add(-3, -3))
+        self.assertEqual(Calculator.add(2, 2), 4)
+        self.assertNotEqual(Calculator.add(2, 2), -4)
+        self.assertEqual(Calculator.add(1, 2.2), 3.2)
+        self.assertEqual(Calculator.add(-3, -3), -6)
 
     def test_sub(self):
-        self.assertEqual(2, self.calculator.sub(3, 1))
-        self.assertEqual(0, self.calculator.sub(-3, -3))
-        self.assertEqual(-2, self.calculator.sub(1, 3))
+        self.assertEqual(Calculator.sub(3, 1), 2)
+        self.assertEqual(Calculator.sub(-3, -3), 0)
+        self.assertEqual(Calculator.sub(1, 3), -2)
 
     def test_mul(self):
-        self.assertEqual(12, self.calculator.mul(3, 4))
-        self.assertEqual(0, self.calculator.mul(3, 0))
-        self.assertEqual(13.5, self.calculator.mul(3, 4.5))
+        self.assertEqual(Calculator.mul(3, 4), 12)
+        self.assertEqual(Calculator.mul(3, 0), 0)
+        self.assertEqual(Calculator.mul(3, 4.5), 13.5)
 
     def test_div(self):
-        self.assertEqual(3, self.calculator.div(9, 3))
-        self.assertEqual(4, self.calculator.div(16, 4))
+        self.assertEqual(Calculator.div(9, 3), 3)
+        self.assertEqual(Calculator.div(16, 4), 4)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
