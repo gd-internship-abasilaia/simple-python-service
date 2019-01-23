17bbcce5ac10e6ed12159cc98888904d8a002f1c
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-01-23 14:18:34 +0300

    Wrapped Python project into setuptools with update README (#8)
    ADSAFE-185
    
    * Added setup.py. More description in README.md
    * Rename package. Now its Python-Calculator. Remove description

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name="Python-Calculator",
+    version="0.2",
+    packages=['calc'],
+    test_suite="tests",
+    scripts=[
+        'calculator_app.py',
+        'README.md', ]
+)
