8ab3255777e9850aec157dd43d2c5073f400d345
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-01-31 12:54:48 +0300

    Update pycodestyle.sh

--- pycodestyle.sh
+++ pycodestyle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 pip install pycodestyle
-pycodestyle --ignore=E501,E722,E704
+pycodestyle --first --ignore=E501,E722,E704 tests/test_calculator.py calc/calculator.py calculator_app.py setup.py service.py
