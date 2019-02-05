25783ecf9a16d43358a8032c04fdc151dcead63d
BasileoLuv <44769194+BasileoLuv@users.noreply.github.com>
2019-02-05 18:27:44 +0300

    Update version.py

--- version.py
+++ version.py
@@ -1,2 +1,7 @@
-from datetime import datetime, date, time 
-SNAPSHOT =  str(datetime.utcnow().time())
+import datetime
+import os
+dt = str(datetime.datetime.utcnow())
+newname = 'dist/Python-Calculator-0.4-'+dt+'.tar.gz'
+os.rename('dist/Python-Calculator-0.4.SNAPSHOT.tar.gz', newname)
+newname2 = 'dist/Python_Calculator-0.4_'+dt+'SNAPSHOT-py2-none-any.whl'
+os.rename('dist/Python_Calculator-0.4.SNAPSHOT-py2-none-any.whl', newname2)
