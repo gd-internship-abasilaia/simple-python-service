56926dbddcf3370bfbed1a8894f360bbc3abbb39
BasileoLuv <idronandrew@gmail.com>
2019-02-08 16:46:46 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup
+from datetime import datetime
+import os
 from setuptools.command.install import install as InstallCommand
-
 
 class Install(InstallCommand):
     """ Customized setuptools install command which uses pip. """
+
     def run(self, *args, **kwargs):
         import pip
         pip.main(['install', '.'])
         InstallCommand.run(self, *args, **kwargs)
 
-
 setup(
     name="Python-Calculator",
-    version="0.2",
+    version="0.4-SNAPSHOT",
     packages=['calc'],
     test_suite="tests",
     scripts=[
         'calculator_app.py',
         'service.py',
-        'README.md', ],
-    install_requires=['Flask']
+        'README.md',
+        'commit.sh', 'newcommit.txt'],
+    install_requires=['Flask'],
 )
